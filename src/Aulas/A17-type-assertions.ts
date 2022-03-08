e910942a73f324b2cb0f100878b99d9587181c9a
derikbf <derikb@gmail.com>
2022-03-08 12:54:46 -0300

    Type assertions - Typescript

--- src/Aulas/A17-type-assertions.ts
+++ src/Aulas/A17-type-assertions.ts
@@ -1,10 +1,8 @@
+/* Recomentado ---------
+
 // Condicional
 const body1 = document.querySelector('body');
 if (body1) body1.style.background = 'red';
-
-// Non-null assertion (!)
-const body2 = document.querySelector('body')!;
-body2.style.background = 'red';
 
 // Type assertion
 const body3 = document.querySelector('body') as HTMLBodyElement;
@@ -15,5 +13,13 @@ const input = document.querySelector('.input') as HTMLInputElement;
 input.value = 'Qualquer coisa';
 input.focus();
 
+---------   ---------  --------- */ 
+
+// Não é recomendado 
+
 // Type assertion
 const body4 = document.querySelector('body') as unknown as number;
+
+// Non-null assertion (!)
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
