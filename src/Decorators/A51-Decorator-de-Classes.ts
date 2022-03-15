66598ee48dd4e8fd9404fc2583781ad5402ffefa
derikbf <derikb@gmail.com>
2022-03-14 22:29:44 -0300

    Secao 22 - Typescript - Decorator de Classe

---
+++ src/Decorators/A51-Decorator-de-Classes.ts
@@ -0,0 +1,27 @@
+export class Animal {
+  constructor(
+    public nome: string, 
+    public cor: string
+  ) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    cor: string;
+    nome: string;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.nome = this.inverte(args[0]);
+      this.cor = this.inverte(args[1]);
+    }
+
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('');
+    }
+  }
+}
+
+const AnimalDecorated = decorator(Animal);
+const animal = new AnimalDecorated('Luiz', 'roxo');
+console.log(animal);
